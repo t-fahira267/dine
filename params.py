11edc3355f2887227bb0005b98d0248b28d90730
Tasha Fahira <fahiratasha@gmail.com>
2026-02-14 20:01:37 +0900

    Separate Env vars to params and local .env file

--- params.py
+++ params.py
@@ -1,10 +1,18 @@
+import os
+
 # =============================
 # Dataset Parameters
 # =============================
 
-DISHES = ["Sushi", "Ramen"]
+DISHES = ["Apple", "Fried Chicken", "Pizza", "Sushi", "Sushi Platter"
+          "Mapo Tofu", "Egg Tart", "Boiled Eggs", "Grilled Steak",
+          "Hamburger", "Oranges"]
 PER_CLASS = 50
-DATASET_VERSION = "v1"
+DATASET_VERSION = os.getenv("DATASET_VERSION")
 
-BASE_DATA_DIR = "data/mmfood100k"
+BASE_DATA_DIR = os.getenv("BASE_DATA_DIR")
 OUTPUT_DIR = f"{BASE_DATA_DIR}/{DATASET_VERSION}"
+
+
+DEPLOYMENT=os.getenv("DEPLOYMENT")
+GCS_BUCKET_NAME="dine-mmfood"
