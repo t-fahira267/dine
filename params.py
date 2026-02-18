8753d50cd39531e23fb62a122820d89287f1a709
Tasha Fahira <fahiratasha@gmail.com>
2026-02-19 08:33:32 +0900

    Delete deployment from env variable params

--- params.py
+++ params.py
@@ -7,12 +7,12 @@ import os
 DISHES = ["Apple", "Fried Chicken", "Pizza", "Sushi", "Sushi Platter"
           "Mapo Tofu", "Egg Tart", "Boiled Eggs", "Grilled Steak",
           "Hamburger", "Oranges"]
-PER_CLASS = 50
+PER_CLASS = 1
 DATASET_VERSION = os.getenv("DATASET_VERSION")
 
 BASE_DATA_DIR = os.getenv("BASE_DATA_DIR")
 OUTPUT_DIR = f"{BASE_DATA_DIR}/{DATASET_VERSION}"
 
 
-DEPLOYMENT=os.getenv("DEPLOYMENT")
+
 GCS_BUCKET_NAME="dine-mmfood"
