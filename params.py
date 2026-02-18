32ae6a525e85c27f945b90eed3c17aa2993b307d
Tasha Fahira <fahiratasha@gmail.com>
2026-02-19 08:52:52 +0900

    Update params

--- params.py
+++ params.py
@@ -1,18 +1,24 @@
 import os
 
 # =============================
+# Local file paths
+# =============================
+
+BASE_DATA_DIR = os.getenv("BASE_DATA_DIR")
+DATASET_OUTPUT_DIR = f"{BASE_DATA_DIR}/"
+
+# =============================
 # Dataset Parameters
 # =============================
 
+SAVE_MODE = "local"  # or "gcs"
 DISHES = ["Apple", "Fried Chicken", "Pizza", "Sushi", "Sushi Platter"
           "Mapo Tofu", "Egg Tart", "Boiled Eggs", "Grilled Steak",
           "Hamburger", "Oranges"]
 PER_CLASS = 1
-DATASET_VERSION = os.getenv("DATASET_VERSION")
 
-BASE_DATA_DIR = os.getenv("BASE_DATA_DIR")
-OUTPUT_DIR = f"{BASE_DATA_DIR}/{DATASET_VERSION}"
-
-
+# =============================
+# Google Cloud Storage Parameters
+# =============================
 
 GCS_BUCKET_NAME="dine-mmfood"
