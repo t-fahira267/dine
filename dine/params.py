772951732a77ddf7148f9abcbbb74f60d64d387c
Tasha Fahira <fahiratasha@gmail.com>
2026-02-19 21:57:05 +0900

    Move params to dine.params for packaging

--- dine/params.py
+++ dine/params.py
@@ -13,7 +13,15 @@ DATASET_OUTPUT_DIR = f"{BASE_DATA_DIR}/"
 
 DISHES = ["apple", "fried chicken", "pizza", "sushi", "ramen", "mapo tofu",
           "egg tart", "boiled eggs", "grilled steak", "hamburger"]
-PER_CLASS = 1
+PER_CLASS = 200
+DATASET_VERSION = "v1"
+
+# --- Only when running `make create-dataset`
+SAVE_MODE = "local"  # or "gcs"
+
+# --- Only when running `make dataset` ---
+OUTPUT_FILENAME = "candidates.csv"
+LABELS_FILENAME = "labels.csv"
 
 # =============================
 # Google Cloud Storage Parameters
